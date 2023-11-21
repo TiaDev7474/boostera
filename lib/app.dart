6dc2515a491de8540543fbdfb23bbd121292bc54
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-11-21 19:35:19 +0300

    chore: adding bloc dependencie

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BossteraApp extends StatelessWidget {
+  const BossteraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
