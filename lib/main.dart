c5e234b4a5b38736efb65eecd5c883d7a2f63450
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-11-22 15:02:07 +0300

    feature: add multi provider and boostera logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:boostera/app.dart';
 import 'package:boostera/src/core/route/app_route.dart';
+import 'package:boostera/src/features/app_overview/repositories/category.repository.dart';
 import 'package:boostera/src/features/event/repositories/event.repo.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -9,6 +10,7 @@ void main(){
   AppRouter.instance;
   GetIt.instance.registerLazySingleton<EventRepositoryImpl>(() => EventRepositoryImpl(client: http.Client()));
   GetIt.instance.registerLazySingleton<EventRepositoryProvider>(() => EventRepositoryProvider(GetIt.instance<EventRepositoryImpl>()));
+  GetIt.instance.registerLazySingleton<CategoryRepository>(()=>CategoryRepository());
   runApp(const BossteraApp());
 }
 
---
+++ lib/src/common/widget/logo.widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class BoosteraLogo extends StatelessWidget {
+  const BoosteraLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Boostera.',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.black),);
+  }
+}
