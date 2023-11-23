c28d359bd1d0e4a3caf29399ab6fd14d9f5beede
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-11-23 20:28:37 +0300

    feature: add like and unlike fonctionality in destinatoin section

--- lib/src/features/app_overview/bloc/bloc/like.blloc.dart
+++ lib/src/features/app_overview/bloc/bloc/like.blloc.dart
@@ -4,8 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LikeBloc extends Bloc<LikeEvent,LikeState>{
   LikeBloc() : super(LikeState(destinationLikedIds: [])){
+    List<int> destinationIds= state.destinationLikedIds;
     on<LikePressedEvent>((event,emit)async{
-
+       if (state.destinationLikedIds.contains(event.payload.destinationId)){
+          destinationIds.remove(event.payload.destinationId);
+       }else{
+         destinationIds.add(event.payload.destinationId);
+       }
+       emit(LikeState(destinationLikedIds: destinationIds));
     });
     on<LikeInitialiseEvent>((event,emit)async{
        List<int> destinationIds= state.destinationLikedIds;
